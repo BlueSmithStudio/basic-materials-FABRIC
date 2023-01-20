934d44d26d6f5285f037eafc06ae49f3a2b11943
BlueSmithStudio <daschaos@outlook.com>
2023-01-20 14:23:15 +0100

    Initial commit

---
+++ src/main/java/bluesmith/basicmaterials/BasicMaterialsClient.java
@@ -0,0 +1,10 @@
+package bluesmith.basicmaterials;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicMaterialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bluesmith/basicmaterials/BasicMaterialsDataGenerator.java
@@ -0,0 +1,11 @@
+package bluesmith.basicmaterials;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BasicMaterialsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
